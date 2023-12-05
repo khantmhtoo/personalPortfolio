528570147098b753a748d16b004f07e849513e3f
4Chan V <khantmhtoo1294@gmail.com>
2023-12-04 23:03:24 -0800

    add project

---
+++ frontend_v1/src/app/components/blog/blogdetail/blogdetail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogdetail',
+  standalone: true,
+  imports: [],
+  templateUrl: './blogdetail.component.html',
+  styleUrl: './blogdetail.component.scss'
+})
+export class BlogdetailComponent {
+
+}
---
+++ frontend_v1/src/app/routes/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+
+export const APP_ROUTES: Routes = [
+    {    
+        path: "",
+        redirectTo: 'dashboard',
+        pathMatch: 'full'},
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+];
