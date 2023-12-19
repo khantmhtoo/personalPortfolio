a6caec8eba12b596b2f7dbea083625a783b76a51
4Chan V <khantmhtoo1294@gmail.com>
2023-12-19 01:13:35 -0800

    removed v1 || completed the theme change

--- frontend_v1/src/app/components/dashboard/dashboard.component.ts
+++ frontend_v1/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  skillRotate = [
+    {
+      id: 1,
+      skill: 'frontend developer',
+    },
+    {
+      id: 2,
+      skill: 'backend developer',
+    },
+    {
+      id: 3,
+      skill: 'fullstack developer',
+    },
+    {
+      id: 4,
+      skill: 'game developer',
+    },
+  ];
+  currentIndex = 1;
+  animationDuration = 1; // Change this to adjust animation speed
 
+  private slideChange$ = interval(2000); // Change this to adjust slide interval
+
+  ngOnInit(): void {
+    this.slideChange$.subscribe(() => {
+      this.currentIndex =
+        (this.currentIndex + 1) % (this.skillRotate.length + 1);
+    });
+  }
 }
