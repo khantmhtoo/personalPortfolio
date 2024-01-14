a7dfaabc7cd20ffe06c15502e8ccfe80f1db2cdf
4Chan V <khantmhtoo1294@gmail.com>
2024-01-14 04:15:40 -0800

    rough finish on projectlist

--- frontend_v1/src/app/routes/app.routes.ts
+++ frontend_v1/src/app/routes/app.routes.ts
@@ -2,12 +2,34 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
 export const APP_ROUTES: Routes = [
-    {    
-        path: "",
-        redirectTo: 'dashboard',
-        pathMatch: 'full'},
-    {
-        path: 'dashboard',
-        component: DashboardComponent
-    },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import('../components/blog/blog.component').then(
+        (mod) => mod.BlogComponent
+      ),
+  },
+  {
+    path: 'project',
+    loadComponent: () =>
+      import('../components/project/project.component').then(
+        (mod) => mod.ProjectComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('../cores/components/error/error.component').then(
+        (mod) => mod.ErrorComponent
+      ),
+  },
 ];
