563180e912b9020d22b4b09347e891398eeb5791
4Chan V <khantmhtoo1294@gmail.com>
2024-01-19 23:25:19 -0800

    more minor fix

--- frontend_v1/src/app/components/contact/contact.component.ts
+++ frontend_v1/src/app/components/contact/contact.component.ts
@@ -4,27 +4,42 @@ import {
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
+import { NotificationService } from '../../cores/services/notification.service';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../cores/services/theme.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent implements OnInit {
   _fb = inject(FormBuilder);
+  _noti = inject(NotificationService);
+  _theme = inject(ThemeService);
   form!: FormGroup;
+  currThemeDark?: any = this._theme.signalThemeDark();
 
   ngOnInit(): void {
     this.form = this._fb.group({
-      name: '',
-      email: '',
-      subject: '',
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', [Validators.required]],
     });
   }
+
   onSend() {
-    console.log('Email has been send.');
+    if (this.form.valid) {
+      console.log('Email has been send.');
+      this.form.reset();
+      // Just use notification to update the user if the process is completed or not
+    } else {
+      console.log('Email is not valid');
+      // Just use notification to update the user if the process is completed or not
+    }
   }
 }
